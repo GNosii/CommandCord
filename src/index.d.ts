3a1acfe46ffe8eac2eb07103f24313fc1ee8be23
Gabriel Arriagada <65878291+GNosii@users.noreply.github.com>
2022-05-26 01:06:42 +0000

    improve type definition of deploy function

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,7 @@ import { InitialObject } from "./internal";
 
 /**
  * Deploy an object with valid CommandCord commands to Discord's API.
- * @param {object} content The commands that we should deploy.
+ * @param {InitialObject} content The commands that we should deploy.
  * @param {string} id The ID of your application as an string.
  * @param {string} token The token of your bot. Used for authentication with Discord.
  * @param {string} guild The ID of the development guild, if undefined, commands will be globally deployed.
